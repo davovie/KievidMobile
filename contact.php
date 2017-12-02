9ed3a39e1f36d1c694d1c161138f5a964d99bb76
Ozi Oviezibe David <davovie@gmail.com>
2017-12-02 08:34:03 +0100

    Add project files.

---
+++ contact.php
@@ -0,0 +1,29 @@
+﻿<?php
+
+if($_POST["submit"]) {
+    $recipient="david_ozi@kievidmobile.com";
+    $subject="Form to email message";
+    $sender=$_POST["sender"];
+    $senderEmail=$_POST["senderEmail"];
+    $message=$_POST["message"];
+
+    $mailBody="Name: $sender\nEmail: $senderEmail\n\n$message";
+
+    mail($recipient, $subject, $mailBody, "From: $sender <$senderEmail>
+    ");
+
+    $thankYou="<p>Thank you! Your message has been sent.</p>";
+    }
+
+    ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title></title>
+</head>
+<body>
+
+</body>
+</html>
